3a1ec29a5e4f32f7fcded072317adfc30301010c
DEMOHCHIK <smnvmaks@gmail.com>
2024-01-16 14:42:53 +0600

    Реализовал просмотр, редактирование, добавление, удаление постов

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from webapp.views import home
+from webapp.views import home, PostDetailView, PostUpdateView, PostCreateView, PostDeleteView
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', home),
-    path('home/', home, name='home')
+    path('home/', home, name='home'),
+    path('post/create/', PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.utils import timezone
 from django.utils.timesince import timesince
+from django.views.generic import DetailView, UpdateView, CreateView, DeleteView
 
+from webapp.forms import PostForm
 from webapp.models import Post
 
 
@@ -12,3 +15,39 @@ def home(request):
         post.time_since_post = timesince(post.pub_date, timezone.now())
 
     return render(request, 'home.html', {'posts': posts})
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_create.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.save()
+        return redirect('webapp:post_detail', pk=post.pk)
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('webapp:post_detail', kwargs={'pk': self.object.pk})
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('webapp:home')
+
+    def get_cancel_url(self):
+        return reverse_lazy('webapp:post_detail', kwargs={'pk': self.object.pk})
