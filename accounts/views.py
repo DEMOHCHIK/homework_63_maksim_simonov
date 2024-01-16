74f883ec1e632f76a072866d6d740dfe3b4dd26c
DEMOHCHIK <smnvmaks@gmail.com>
2024-01-16 12:54:32 +0600

    Сделал провиль пользователя

--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,19 @@ from django.urls import reverse
 from django.views.generic import DetailView, CreateView, ListView, FormView
 
 from accounts.forms import MyUserCreationForm, UserSearchForm
+from webapp.models import Post
 
 
-class UserProfileDetailView(DetailView):
+class UserDetailView(DetailView):
     model = get_user_model()
     template_name = 'user_detail.html'
     context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        context['posts'] = Post.objects.filter(author=user)
+        return context
 
 
 class RegisterView(CreateView):
